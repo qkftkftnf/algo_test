45e83a375e30abd1ee7b7946aaefaf2db51d87db
qkftkftnf <65700603+qkftkftnf@users.noreply.github.com>
2021-08-04 13:48:53 +0900

    1001 A - B

---
+++ 1/1001.java
@@ -0,0 +1,16 @@
+package JOON;
+
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+			
+		Scanner scan = new Scanner(System.in);
+		int a=scan.nextInt();
+		int b=scan.nextInt();
+		
+		int result= a-b;
+
+		System.out.println(result);
+	}
+}
