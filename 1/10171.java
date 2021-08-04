9931086f752b6c78e1a55e08f7556a94fa1b2ee2
qkftkftnf <65700603+qkftkftnf@users.noreply.github.com>
2021-08-04 13:48:03 +0900

    10171 cat

---
+++ 1/10171.java
@@ -0,0 +1,11 @@
+package JOON;
+
+
+public class Main {
+	public static void main(String[] args) {
+			System.out.println("\\    /\\");
+			System.out.println(" )  ( ')");
+			System.out.println("(  /  )");
+			System.out.println(" \\(__)|");				
+	}
+}
