af7375403097cdf0c0977becf182fea388a0ee4c
qkftkftnf <65700603+qkftkftnf@users.noreply.github.com>
2021-08-04 13:51:40 +0900

    1008 A / B

---
+++ 1/1008.java
@@ -0,0 +1,15 @@
+package JOON;
+
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+			
+		Scanner scan = new Scanner(System.in);
+		double a=scan.nextInt();
+		double b=scan.nextInt();
+		
+		double result= a/b;
+		System.out.println(result);
+	}
+}
