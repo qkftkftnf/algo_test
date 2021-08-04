3fd05d6b8f7c930795dad2212ed98c041eeae424
qkftkftnf <65700603+qkftkftnf@users.noreply.github.com>
2021-08-04 14:01:23 +0900

    10430 compare

---
+++ 1/10430.java
@@ -0,0 +1,18 @@
+package JOON;
+
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+			
+		Scanner scan = new Scanner(System.in);
+		int a=scan.nextInt();
+		int b=scan.nextInt();
+		int c=scan.nextInt();
+		
+		System.out.println((a + b) % c);
+		System.out.println(((a%c) + (b%c)) % c);
+		System.out.println((a*b) % c);
+		System.out.println(((a%c) * (b%c)) % c);		
+	}
+}
