fa5185bd57749ef6883fd1ec29bbcf5ff1edc0fe
qkftkftnf <65700603+qkftkftnf@users.noreply.github.com>
2021-08-04 13:49:17 +0900

    10998 A X B

---
+++ 1/10998.java
@@ -0,0 +1,15 @@
+package JOON;
+
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+			
+		Scanner scan = new Scanner(System.in);
+		int a=scan.nextInt();
+		int b=scan.nextInt();
+		
+		int result= a*b;
+		System.out.println(result);
+	}
+}
