8a0708204164c288bedb6a05eca9475170e58272
qkftkftnf <65700603+qkftkftnf@users.noreply.github.com>
2021-08-05 15:29:00 +0900

    1330 compare two numbers

---
+++ 2/1330.java
@@ -0,0 +1,24 @@
+package JOON;
+
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+			
+		Scanner scan = new Scanner(System.in);
+		int a=scan.nextInt();
+		int b=scan.nextInt();
+
+		if(a<b) {
+		System.out.println("<");
+		}
+		
+		else if(a>b) {
+			System.out.println(">");
+		}
+		
+		else {
+		System.out.println("==");
+		}		
+	}
+}
