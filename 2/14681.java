a32af0864c2c6a7dd51a5f74139ca968cca7393a
qkftkftnf <65700603+qkftkftnf@users.noreply.github.com>
2021-08-05 15:43:09 +0900

    14681 choose quaternion

---
+++ 2/14681.java
@@ -0,0 +1,25 @@
+package JOON;
+
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+			
+		Scanner scan = new Scanner(System.in);
+		int a=scan.nextInt();
+		int b=scan.nextInt();
+		
+		if(a>0 && b>0){
+			System.out.println("1");
+		}	
+		else if(a<0 && b>0) {
+			System.out.println("2");	
+		}	
+		else if(a<0 && b<0) {
+			System.out.println("3");	
+		}
+		else {
+			System.out.println("4");
+		}
+	}
+}
