260a15076d2c8e1df5da5a1c84431fd6113075b7
qkftkftnf <65700603+qkftkftnf@users.noreply.github.com>
2021-08-08 21:20:42 +0900

    15552 fast A + B

---
+++ 3/15552.java
@@ -0,0 +1,20 @@
+package JOON;
+
+
+import java.io.*;
+import java.util.*;
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int a=Integer.parseInt(br.readLine());
+
+		
+		for(int i=1;i<=a;i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			bw.write(Integer.parseInt(st.nextToken())+Integer.parseInt(st.nextToken()) + "\n");  
+		}		
+		bw.close();
+		
+	}
+}
