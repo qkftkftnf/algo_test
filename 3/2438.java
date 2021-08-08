b52ffc63e73a6aacda2bd8e6fe3218ac92eec727
qkftkftnf <65700603+qkftkftnf@users.noreply.github.com>
2021-08-08 21:33:21 +0900

    2438 print star

---
+++ 3/2438.java
@@ -0,0 +1,17 @@
+package JOON;
+
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int a = scan.nextInt();
+
+		for(int i=1;i<=a;i++){
+			for(int j=0;j<i;j++) {
+				System.out.print("*");
+			}
+			System.out.println("");
+		}
+	}
+}
