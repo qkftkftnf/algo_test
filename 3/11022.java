6ff5ef0e82576d276db65cf3e344ada9cb91b03c
qkftkftnf <65700603+qkftkftnf@users.noreply.github.com>
2021-08-08 21:55:21 +0900

    11022 print Case #:A + B 2

---
+++ 3/11022.java
@@ -0,0 +1,17 @@
+package JOON;
+
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int a = scan.nextInt();
+
+		for(int i=1;i<=a;i++){
+			int b = scan.nextInt();
+			int c = scan.nextInt();
+			int sum=b+c;
+			System.out.println("Case #" + i +": "+ b +" + "+ c +" = "+ sum);
+		}		
+	}
+}
