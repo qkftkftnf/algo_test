54d1b6699411714b2e887c159b71234b8e16e4fb
qkftkftnf <65700603+qkftkftnf@users.noreply.github.com>
2021-08-08 20:52:51 +0900

    10950 for & A + B

---
+++ 3/10950.java
@@ -0,0 +1,17 @@
+package JOON;
+
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+			
+		Scanner scan = new Scanner(System.in);
+		int a=scan.nextInt();
+
+		for(int i=0;i<a;i++) {
+			int b=scan.nextInt();
+			int c=scan.nextInt();
+			System.out.println(b+c);
+		}		
+	}
+}
