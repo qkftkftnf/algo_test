5ac50e936fd1eee4a0ec850fd15e4736ef50ee5a
qkftkftnf <65700603+qkftkftnf@users.noreply.github.com>
2021-08-08 21:25:30 +0900

    2741 print 1~N

---
+++ 3/2741.java
@@ -0,0 +1,14 @@
+package JOON;
+
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int a = scan.nextInt();
+
+		for(int i=1;i<=a;i++){
+			System.out.println(i);
+		}		
+	}
+}
