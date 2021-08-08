c238ede38e9009bf985646be64ad9d1420517378
qkftkftnf <65700603+qkftkftnf@users.noreply.github.com>
2021-08-08 21:27:15 +0900

    2742 print N~1

---
+++ 3/2742.java
@@ -0,0 +1,14 @@
+package JOON;
+
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int a = scan.nextInt();
+
+		for(int i=a;i>0;i--){
+			System.out.println(i);
+		}		
+	}
+}
