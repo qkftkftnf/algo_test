c2df3906df4592afeb541d28f51028e460ce4e1e
qkftkftnf <65700603+qkftkftnf@users.noreply.github.com>
2021-08-09 17:29:37 +0900

    10951 while & A + B2

---
+++ 4/10951.java
@@ -0,0 +1,18 @@
+package JOON;
+
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int b=1,c=1;
+
+		while(scan.hasNext()) {
+			b = scan.nextInt();
+			c = scan.nextInt();
+			int sum=b+c;
+		
+			System.out.println(sum);	
+		}
+	}
+}
