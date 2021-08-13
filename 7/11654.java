5bc7460a67ff3c907f8c91adb33e05a9b503467a
qkftkftnf <65700603+qkftkftnf@users.noreply.github.com>
2021-08-13 18:23:46 +0900

    11654 ASCII code

---
+++ 7/11654.java
@@ -0,0 +1,15 @@
+package JOON;
+
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		char a= scan.next().charAt(0);
+		
+		int asc=a;
+		
+		System.out.println(asc);
+		
+	}	
+}
