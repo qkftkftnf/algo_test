d30877c6ba4cc3938d2dde99086f18b56aee987f
qkftkftnf <65700603+qkftkftnf@users.noreply.github.com>
2021-08-19 18:38:04 +0900

    1712 break even point

---
+++ 8/1712.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int a = scan.nextInt();
+		int b = scan.nextInt();
+		int c = scan.nextInt();
+
+		
+		if(b>=c) {
+			System.out.println("-1");
+		}else {
+			System.out.println((a/(c-b))+1);
+		}
+	
+	}
+}
