7d728c087424b2de745a3c2fd87d89630535621b
박상수 <qkftkftnf@naver.com>
2022-07-24 13:53:01 +0900

    동전0

---
+++ 2022-07/11047.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static int N,K,arr[],min=Integer.MAX_VALUE;
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+
+	
+		N=sc.nextInt();
+		K=sc.nextInt();
+		
+		arr=new int [N];
+		
+		for(int i=0;i<N;i++) {
+			arr[i]=sc.nextInt();
+			
+		}
+		
+		greedy(0,0,N-1);
+		System.out.println(min);
+		
+				
+	}
+	public static void greedy(int sum,int count,int pos) {
+		if(sum==K){
+			min=count;
+			return;
+		}else if(arr[pos]>K-sum) {
+			greedy(sum,count,pos-1);
+		}else {
+			int num=(K-sum)/arr[pos];
+			greedy(sum+(num*arr[pos]),count+num,pos-1);
+		}
+	}
+
+}
