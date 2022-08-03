a3d47f9082301c296a5656a67d18ad1fc43d39e6
박상수 <qkftkftnf@naver.com>
2022-08-03 20:27:39 +0900

    Add: 동전1

---
+++ 2022-08/2293.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		
+		int n=sc.nextInt();
+		int k=sc.nextInt();
+		
+		int dp[]=new int[k+1];
+		int coin[]=new int[n];
+		for(int i=0;i<n;i++) {
+			coin[i]=sc.nextInt();
+		}
+		
+		
+		dp[0]=1;
+		
+		for(int i=0;i<n;i++) {
+			for(int j=1;j<=k;j++) {
+				if(j>=coin[i]) {
+					dp[j]+=dp[j-coin[i]];
+				}
+			}
+		}
+		System.out.println(dp[k]);
+		
+
+	}
+
+}
