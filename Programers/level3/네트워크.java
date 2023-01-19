15de617a08bff892bdd4603f6293659024f82d4b
박상수 <qkftkftnf@naver.com>
2023-01-19 18:52:07 +0900

    Add: 네트워크

---
+++ Programers/level3/네트워크.java
@@ -0,0 +1,26 @@
+class Solution {
+    public static boolean visited[];
+    public int solution(int n, int[][] computers) {
+        int answer = 0;
+        
+        visited=new boolean[n];
+        for(int i=0;i<n;i++){
+            if(!visited[i]){
+                dfs(i,computers);
+                answer++;
+            } 
+        }
+        return answer;
+    }
+    
+    
+    public static void dfs(int i,int computers[][]){
+        visited[i]=true;
+        
+        for(int j=0;j<computers[i].length;j++){
+            if(computers[i][j]==1 && !visited[j]){
+                dfs(j,computers);
+            }
+        }
+    }
+}
