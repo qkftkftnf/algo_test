ee1f5100f026dff78d54b0eb11a78df3b9caad41
박상수 <qkftkftnf@naver.com>
2023-02-10 13:25:41 +0900

    FIX : 숫자 블록 체점 기준 바뀜

--- Programers/level2/숫자 블록.java
+++ Programers/level2/숫자 블록.java
@@ -4,14 +4,16 @@ class Solution {
         int[] answer = new int[len+1];
         
         for(int i=0;i<=len;i++){
-            
             int num=(int)Math.sqrt(i+begin);
             boolean c=true;
             for(int j=2;j<=num;j++){
-                if((i+begin)%j==0 && (i+begin)/j<=10000000 ) {
-                    answer[i]=(int)(i+begin)/j;
+                if((i+begin)%j==0 ) {
+                    if( (i+begin)/j<=10000000){
+                        answer[i]=Math.max(answer[i],(int)(i+begin)/j);
+                    }else{
+                        answer[i]=j;
+                    }
                     c=false;
-                    break;
                 }
             }
             if(c && i+begin>1)answer[i]=1;
