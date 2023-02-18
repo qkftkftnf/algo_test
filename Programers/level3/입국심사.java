f6ae18e33e424b4ec4251dbdd8b18f6797eec32d
박상수 <qkftkftnf@naver.com>
2023-02-18 16:36:29 +0900

    Add: 입국심사

---
+++ Programers/level3/입국심사.java
@@ -0,0 +1,22 @@
+class Solution {
+    public long solution(int n, int[] times) {
+        long answer = 0;
+        
+        long min=0;
+        long max=(long)n*times[times.length-1];
+        
+        while(min<=max){
+            long mid=(min+max)/2;
+            long sum=0;
+            for(int i=0;i<times.length;i++){
+                sum+=(mid/times[i]);
+            }
+            if(sum<n) min=mid+1;
+            else{
+                max=mid-1;
+                answer=mid;
+            }
+        }
+        return answer;
+    }
+}
